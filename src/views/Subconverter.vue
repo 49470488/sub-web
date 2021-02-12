511e35164bfc7213e0bd11ebd788b5fb592ff1a1
49470488 <71637330+49470488@users.noreply.github.com>
2021-02-12 20:15:10 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -268,7 +268,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "nas.kkumo.tk (kumo测试）": "https://sub.kumosub.tk/sub?",
+          "sub.kumosub.tk (kumo测试）": "https://sub.kumosub.tk/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
